935062b1c8055f1c4eba2a302ecc1d9c4def62d8
wjpp <waynepope@gmail.com>
2015-09-02 16:30:01 +0100

    Initial commit in VS

---
+++ Sync1/DiskItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sync1
+{
+    public class DiskItem
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public string FileName { get; set; }
+        public int Size { get; set; }
+        public int Version { get; set; }
+        public int FolderId { get; set; }
+        public string Action { get; set; }
+        public int WorkspaceId { get; set; }
+    }
+}
---
+++ Sync1/Workspace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SQLite;
+
+namespace Sync1
+{
+    class Workspace
+    {
+        public bool WritePermission  { get; set; }
+        public string Name { get; set; }
+        [PrimaryKey]
+        public int Id { get; set; }
+    }
+}
