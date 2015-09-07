db5a0be709b33d928a918e090aa91ece1446e49b
wjpp <waynepope@gmail.com>
2015-09-07 19:08:15 +0100

    One way sync part1

--- Sync1/Globals.cs
+++ Sync1/Globals.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SQLite;
+
 namespace Sync1
 {
     class Globals
     {
+        [PrimaryKey]
+        public int Id { get; set; }
         public int CurrentRevision { get; set; }
     }
 }
--- Sync1/SyncMain.cs
+++ Sync1/SyncMain.cs
@@ -10,6 +10,7 @@ using System.IO;
 using SQLite;
 using System.Diagnostics;
 using System.Net;
+using System.Threading;
 
 namespace Sync1
 {
@@ -20,6 +21,7 @@ namespace Sync1
         const string apiId = "11111";
         const string rootDir = "C:\\test";
         const string server = "http://home.glasscubesdev.com:8080/";
+        const string dbName = "sync3.db";
         private SQLiteConnection db;
 
         static void Main(string[] args)
@@ -27,35 +29,105 @@ namespace Sync1
             SyncMain p = new SyncMain();
             p.Start();
 
-
-
-
- //               var request = new RestRequest();
- //             request.Resource = "/rest/sync/hello";
-
-            //           IRestResponse response = client.Execute(request);
-
         }
 
         public void Start()
         {
             client.BaseUrl = new Uri(server);
-            Init();
+            
+            DbConnect(dbName);
+            var global = db.Find<Globals>(1);
+            if (global == null)
+            {
+                Init();
+            } 
 
+            while (true)
+            {
+
+                Thread.Sleep(2000);
+                CheckForChanges();
+            }
         }
 
+        private void CheckForChanges()
+        {
+            var global = db.Find<Globals>(1);
+            var request = new RestRequest();
+            request.Resource = "/rest/sync/changes";
+            SetUpRequest(request);
+            request.AddParameter("rev", global.CurrentRevision);
+
+            var response = client.Execute<SyncList>(request);
+            if (response.ResponseStatus == ResponseStatus.Completed)
+            {
+                SyncList list = response.Data;
+                List<DiskItem> items = list.Items;
+
+                if (list.LatestRevision > global.CurrentRevision)
+                {
+                    global.CurrentRevision = list.LatestRevision;
+                    db.Update(global);
+
+                    foreach (DiskItem i in items)
+                    {
+                        switch(i.Action)
+                        {
+                            case "ADD":
+                                NewFileAdded(i);
+                                break;
+                            case "DELETED":
+                                DeleteDiskItem(i);
+                                break;
+                            case "UPDATED":
+                               // db.Update(i);
+                               // DownLoadFile(i);
+                                break;
+                            case "RENAMED":
+                        
+                                break;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void NewFileAdded(DiskItem i)
+        {
+            if (i.Folder)
+            {
+                string newPath = Path.Combine(GetRootPath(i), i.FileName);
+                System.IO.Directory.CreateDirectory(newPath);
+                i.Path = newPath;
+                db.Insert(i);
+            } else
+            {
+                db.Insert(i);
+                DownLoadFile(i);
+            }
+        }
+
+        private void DeleteDiskItem(DiskItem i)
+        {
+            db.Delete(i);
+            var path = Path.Combine(GetRootPath(i), i.FileName);
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
 
         private void Init()
         {
-            SetupDB("sync3.db");
+            SetupDB(dbName);
             GetWorkspaces();
             GetInitFiles();
         }
 
-        private void SetupDB(string DatabaseName)
+        private void SetupDB(string databaseName)
         {
-            string folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            db = new SQLiteConnection(System.IO.Path.Combine(folder, DatabaseName));
+           
             db.DropTable<Workspace>();
             db.DropTable<DiskItem>();
             db.DropTable<Globals>();
@@ -64,6 +136,12 @@ namespace Sync1
             db.CreateTable<DiskItem>();
             db.CreateTable<Globals>();
 
+        }
+
+        private void DbConnect(string databaseName)
+        {
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            db = new SQLiteConnection(System.IO.Path.Combine(folder, databaseName));
         }
 
         private void GetInitFiles()
@@ -81,6 +159,7 @@ namespace Sync1
                 List<DiskItem> items = list.Items;
 
                 Globals g = new Globals();
+                g.Id = 1;
                 g.CurrentRevision = list.LatestRevision;
                 db.Insert(g);
                 
@@ -112,32 +191,40 @@ namespace Sync1
                 var files = db.Query<DiskItem>("select * from DiskItem where  Folder = ?", false);
                 foreach(DiskItem f in files)
                 {
-          
-                    string path;
-                    if (f.FolderId > 0)
-                    {
-                        DiskItem parent = db.Find<DiskItem>(f.FolderId);
-                        path = parent.Path;
-                    } else
-                    {
-                        Workspace w = db.Find<Workspace>(f.WorkspaceId);
-                        path = Path.Combine(rootDir, w.Name);
-                    }
-
-                    using (var client = new WebClient())
-                    {
-                        string url = server + "/rest/sync/get?key=" + key + "&apiId=" + apiId + "&docId=" + f.Id;
-                        client.DownloadFile(url, Path.Combine(path, f.FileName));
-                    }
-
-
+                    DownLoadFile(f);
                 }
 
             }
 
         }
 
-     
+        private void DownLoadFile(DiskItem f)
+        {
+            var path = GetRootPath(f);
+
+            using (var client = new WebClient())
+            {
+                string url = server + "rest/sync/get?key=" + key + "&apiId=" + apiId + "&docId=" + f.Id;
+                client.DownloadFile(url, Path.Combine(path, f.FileName));
+            }
+        }
+
+        private string GetRootPath(DiskItem f)
+        {
+            string path;
+            if (f.FolderId > 0)
+            {
+                DiskItem parent = db.Find<DiskItem>(f.FolderId);
+                path = parent.Path;
+            }
+            else
+            {
+                Workspace w = db.Find<Workspace>(f.WorkspaceId);
+                path = Path.Combine(rootDir, w.Name);
+            }
+
+            return path;
+        }
 
         private void createChildFolders(DiskItem item, string path)
         {
