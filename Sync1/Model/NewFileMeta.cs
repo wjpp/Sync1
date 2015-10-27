85e4ded2ab62bf758764181a1831e02bbe08a1e1
wjpp <waynepope@gmail.com>
2015-10-27 15:29:09 +0000

    Missing files

---
+++ Sync1/Model/NewFileMeta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glasscubes.Drive.Model
+{
+    class NewFileMeta
+    {
+        public bool success { get; set; }
+        public long globalRevision { get; set; }
+        public long docId { get; set; }
+        public int version { get; set; }
+        public string checksum { get; set; }
+        public DateTime created { get; set; }
+        public DateTime updated { get; set; }
+    }
+}
